2aa5fbf61025dec144fa8f1adc2a30b09d8be959
Virus <zxcv.fghj.uiop@gmail.com>
2022-03-19 15:43:09 -0400

    Add vm labels

--- src/test/ApeMev.t.sol
+++ src/test/ApeMev.t.sol
@@ -7,8 +7,16 @@ import "../ApeMev.sol";
 
 contract ApeMevTest is BaseTest {
 
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address private constant APECOIN = 0x4d224452801ACEd8B2F0aebE155379bb5D594381;
+    address private constant BAYC_NFTX_VAULT = 0xEA47B64e1BFCCb773A0420247C0aa0a3C1D2E5C5;
+    address private constant BAYC_ERC721 = 0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D;
+
     function setUp() public {
-        vm.label(0xEA47B64e1BFCCb773A0420247C0aa0a3C1D2E5C5, "BAYC_NFTX_VAULT");
+        vm.label(WETH, "WETH");
+        vm.label(APECOIN, "APECOIN");
+        vm.label(BAYC_NFTX_VAULT, "BAYC_NFTX_VAULT");
+        vm.label(BAYC_ERC721, "BAYC_ERC721");
     }
 
     function test_ApeMev() public {
